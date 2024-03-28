68e3a061aef95ec371fc8a3b198786bc165c9e5d
Bara03 <barabas.balazs03@gmail.com>
2024-03-28 16:48:24 +0100

    correction

--- extractsvg.py
+++ extractsvg.py
@@ -18,18 +18,19 @@ def extract_svg_values(svg_file):
 
 if __name__ == "__main__":
     svg_file = find_svg_file()
-    if svg_file is None:
-        print("NO SVG file in the directory.")
-    else:
+    if svg_file:
+        icon_name = input("icon name: ")
+        icon_name = icon_name[:1].upper() + icon_name[1:]
         d, viewBox = extract_svg_values(svg_file)
-        returnValue = f'export const Svg{input("icon name: ").capitalize()} = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>((props, ref: Ref<SVGSVGElement>) => {{\n' \
-                      f'\treturn (\n' \
-                      f'\t\t<svg ref={{ref}} xmlns="http://www.w3.org/2000/svg" viewBox="{viewBox}" {{...props}}>\n' \
-                      f'\t\t\t <path d="{d}" />\n' \
-                      f'\t\t</svg>\n' \
-                      f'\t);\n' \
-                      f'}});'
+        returnValue = f'export const Svg{icon_name} = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>((props, ref: Ref<SVGSVGElement>) => {{\n' \
+                        f'\treturn (\n' \
+                        f'\t\t<svg ref={{ref}} xmlns="http://www.w3.org/2000/svg" viewBox="{viewBox}" {{...props}}>\n' \
+                        f'\t\t\t <path d="{d}" />\n' \
+                        f'\t\t</svg>\n' \
+                        f'\t);\n' \
+                        f'}});'
         print(returnValue)
 
         print("\n\n Copied to clipboard")
         pyperclip.copy(returnValue)
+    else: print("NO SVG file in the directory.")
--- run.bat
+++ run.bat
@@ -4,6 +4,17 @@ setlocal
 REM Set the path to your Python executable
 set PYTHON_PATH=python
 
+REM Check if required Python libraries are available, if not, install them
+%PYTHON_PATH% -c "import xml.etree.ElementTree" 2>nul || (
+    echo Installing required libraries...
+    %PYTHON_PATH% -m pip install xml.etree.ElementTree
+)
+
+%PYTHON_PATH% -c "import pyperclip" 2>nul || (
+    echo Installing required libraries...
+    %PYTHON_PATH% -m pip install pyperclip
+)
+
 REM Run the Python script
 %PYTHON_PATH% extractsvg.py
 
